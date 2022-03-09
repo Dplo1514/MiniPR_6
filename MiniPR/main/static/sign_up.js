dfad5a6721ee174d794c5ea32cbc8b1320fa7c0a
zzpjw <zzpjw9710@naver.com>
2022-03-09 16:49:24 +0900

    sign_up 최종

--- MiniPR/main/static/sign_up.js
+++ MiniPR/main/static/sign_up.js
@@ -3,7 +3,8 @@ function sign_up() {
     let username = $("#input-username").val()
     let password = $("#input-password").val()
     let password2 = $("#input-password2").val()
-    console.log(username, password, password2)
+    let email = $("#input-email").val()
+    console.log(username, password, password2, email)
 
 
     if ($("#help-id").hasClass("is-danger")) {                    //아이디 중복 확인에서 막히면
@@ -11,6 +12,14 @@ function sign_up() {
         return;
     } else if (!$("#help-id").hasClass("is-success")) {           //아이디 중복 확인을 통과하면
         alert("아이디 중복확인을 해주세요.")
+        return;
+    }
+
+    if ($("#help-email").hasClass("is-danger")) {                    //이메일 중복 확인에서 막히면
+        alert("이메일을 다시 확인해주세요.")
+        return;
+    } else if (!$("#help-email").hasClass("is-success")) {           //이메일 중복 확인을 통과하면
+        alert("이메일 중복확인을 해주세요.")
         return;
     }
 
@@ -35,13 +44,15 @@ function sign_up() {
         return;
     } else {
         $("#help-password2").text("비밀번호가 일치합니다.").removeClass("is-danger").addClass("is-success")
-    }                                       //통과한다면 서버로 POST
+    }                                      //통과한다면 서버로 POST
+
     $.ajax({
         type: "POST",
         url: "/sign_up/save",
         data: {
             username_give: username,
-            password_give: password
+            password_give: password,
+            email_give: email
         },
         success: function (response) {
             alert("회원가입을 축하드립니다!")
@@ -59,6 +70,7 @@ function toggle_sign_up() {
     $("#help-id").toggleClass("is-hidden")
     $("#help-password").toggleClass("is-hidden")
     $("#help-password2").toggleClass("is-hidden")
+    $("#help-email").toggleClass("is-hidden")
 }
 
 //아이디, 패스워드 정규표현식
@@ -73,8 +85,15 @@ function is_password(asValue) {
     return regExp.test(asValue);
 }
 
+function is_email(asValue) {
+    //()안에 내용은 필수 포함//'\d'는 숫자를 의미함//
+    var regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+//출처: https://epthffh.tistory.com/entry/비밀번호-정규식 [물고기 개발자의 블로그]//
+    return regExp.test(asValue);
+}
+
 //아이디 중복확인 클라이언트
-function check_dup() {
+function check_dup_id() {
     let username = $("#input-username").val()
     console.log(username)
     if (username == "") {                //중복 확인 버튼을 눌렀는데 아무 것도 입력 하지 않았을 때
@@ -90,7 +109,7 @@ function check_dup() {
     $("#help-id").addClass("is-loading")                     //정상적으로 중복 확인 버튼을 눌렀을 때
     $.ajax({
         type: "POST",
-        url: "/sign_up/check_dup",
+        url: "/sign_up/check_dup_id",
         data: {
             username_give: username
         },
@@ -103,6 +122,41 @@ function check_dup() {
                 $("#help-id").text("사용할 수 있는 아이디입니다.").removeClass("is-danger").addClass("is-success")          //중복 확인 버튼을 정상적으로 통과 해야만 회원가입이 //
             }                                                                                                           //이루어 져야 하므로  .addClass("is-success") 추가//
             $("#help-id").removeClass("is-loading")
+
+        }
+    });
+}
+
+//이메일 중복확인 클라이언트
+function check_dup_email() {
+    let email = $("#input-email").val()
+    console.log(email)
+    if (email == "") {                //중복 확인 버튼을 눌렀는데 아무 것도 입력 하지 않았을 때
+        $("#help-email").text("이메일을 입력해주세요.").removeClass("is-safe").addClass("is-danger") //help-email에 텍스트 입력
+        $("#input-email").focus()
+        return;
+    }
+    if (!is_email(email)) {                 //중복 확인 버튼을 눌렀는데 형식이 맞지 않았을 때
+        $("#help-email").text("이메일의 형식을 확인해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#input-email").focus()
+        return;
+    }
+    $("#help-email").addClass("is-loading")                     //정상적으로 중복 확인 버튼을 눌렀을 때
+    $.ajax({
+        type: "POST",
+        url: "/sign_up/check_dup_email",
+        data: {
+            email_give: email
+        },
+        success: function (response) {
+
+            if (response["exists"]) {
+                $("#help-email").text("이미 존재하는 이메일입니다.").removeClass("is-safe").addClass("is-danger")
+                $("#input-email").focus()
+            } else {
+                $("#help-email").text("사용할 수 있는 이메일입니다.").removeClass("is-danger").addClass("is-success")          //중복 확인 버튼을 정상적으로 통과 해야만 회원가입이 //
+            }                                                                                                           //이루어 져야 하므로  .addClass("is-success") 추가//
+            $("#help-email").removeClass("is-loading")
 
         }
     });
